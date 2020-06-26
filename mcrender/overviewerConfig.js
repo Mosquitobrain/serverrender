8e6ff1948a747d7f3e0b123155ba7bc50221785d
Mosquitobrain <isaiah.sloughter@newarkcsd.org>
2020-06-25 23:50:52 -0400

    Added all files

---
+++ mcrender/overviewerConfig.js
@@ -0,0 +1,98 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Mosquitobrain.aternos.me"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1593131936",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Smooth lighting render of Mosquitobrain.aternos.me",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "smooth_lighting",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mosquitobrain.aternos.me",
+            "last_rendertime": 1592954165,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -64,
+                80,
+                236
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -64,
+                80,
+                236
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Smooth Night render of Mosquitobrain.aternos.me",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "smooth_night",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mosquitobrain.aternos.me",
+            "last_rendertime": 1592954165,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -64,
+                80,
+                236
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -64,
+                80,
+                236
+            ],
+            "north_direction": 0
+        }
+    ]
+};
